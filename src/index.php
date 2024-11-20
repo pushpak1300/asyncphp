646066006bc5a54623076bfb636521adffc1eb49
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-21 01:35:46 +0530

    2. Control Timer

--- src/index.php
+++ src/index.php
@@ -10,9 +10,16 @@ $timer = $loop->addPeriodicTimer(1, function () {
     echo 'Tick' . PHP_EOL;
 });
 
-$loop->addTimer(10, function () use ($loop, $timer) {
+$loop->addTimer(3, function () use ($loop, $timer) {
+    echo 'Stopping timer' . PHP_EOL;
+    $timer->
+    $loop->stop();
+});
+
+$loop->addTimer(5, function () use ($loop, $timer) {
     $loop->cancelTimer($timer);
     echo 'Done' . PHP_EOL;
 });
 
 $loop->run();
+
