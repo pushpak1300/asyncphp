d064faa3afd22c3f0815541992a4e99fcba68238
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-21 01:39:21 +0530

    3. Using the Loop autorun

--- src/index.php
+++ src/index.php
@@ -4,22 +4,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use React\EventLoop\Loop;
 
-$loop = Loop::get();
-
-$timer = $loop->addPeriodicTimer(1, function () {
-    echo 'Tick' . PHP_EOL;
+$timer = Loop::addPeriodicTimer(1, function () {
+    echo 'tick!' . PHP_EOL;
 });
 
-$loop->addTimer(3, function () use ($loop, $timer) {
-    echo 'Stopping timer' . PHP_EOL;
-    $timer->
-    $loop->stop();
-});
-
-$loop->addTimer(5, function () use ($loop, $timer) {
-    $loop->cancelTimer($timer);
+Loop::addTimer(10, function () use ($timer) {
+    Loop::cancelTimer($timer);
     echo 'Done' . PHP_EOL;
 });
-
-$loop->run();
-
