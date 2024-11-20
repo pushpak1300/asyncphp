c2bfdc4f710b5435393bd1d7c2e1ef94f444228e
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-21 01:45:20 +0530

    4. How Event Loop Works after Stopping

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
 
 $timer = Loop::addPeriodicTimer(1, function () {
     echo 'tick!' . PHP_EOL;
@@ -11,4 +12,20 @@ $timer = Loop::addPeriodicTimer(1, function () {
 Loop::addTimer(10, function () use ($timer) {
     Loop::cancelTimer($timer);
     echo 'Done' . PHP_EOL;
+});
+
+
+
+$loop = Loop::get();
+$loop->addTimer(3, function () use ($loop) {
+    assert($loop instanceof LoopInterface);
+    echo 'Stopping loop' . PHP_EOL;
+    $loop->stop();
+    echo 'Stopped loop' . PHP_EOL;
+    $loop->run();
+});
+
+$loop->addTimer(4, function () use ($loop) {
+    $loop->stop();
+    echo 'Stopped loop again' . PHP_EOL;
 });
