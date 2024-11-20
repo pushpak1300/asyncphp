5a543958b264ab58f3c9cdb887d11120be88e667
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-21 01:53:09 +0530

    5. Explaint Tick in event loop

--- src/index.php
+++ src/index.php
@@ -5,27 +5,27 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use React\EventLoop\Loop;
 use React\EventLoop\LoopInterface;
 
-$timer = Loop::addPeriodicTimer(1, function () {
-    echo 'tick!' . PHP_EOL;
+function hello($name)
+{
+    Loop::futureTick(function () use ($name) {
+        echo "hello $name\n";
+    });
+
+Loop::addPeriodicTimer(0.2, function () {
+    hello('World');
 });
 
-Loop::addTimer(10, function () use ($timer) {
-    Loop::cancelTimer($timer);
-    echo 'Done' . PHP_EOL;
+Loop::addPeriodicTimer(0.4, function () {
+    hello('Pune');
 });
 
+// Loop::futureTick(function () {
+//     echo "hello 1\n";
+// });
 
+// Loop::futureTick(function () {
+//     echo "hello 2\n";
+// });
 
-$loop = Loop::get();
-$loop->addTimer(3, function () use ($loop) {
-    assert($loop instanceof LoopInterface);
-    echo 'Stopping loop' . PHP_EOL;
-    $loop->stop();
-    echo 'Stopped loop' . PHP_EOL;
-    $loop->run();
-});
+// echo "hello 3\n";
 
-$loop->addTimer(4, function () use ($loop) {
-    $loop->stop();
-    echo 'Stopped loop again' . PHP_EOL;
-});
